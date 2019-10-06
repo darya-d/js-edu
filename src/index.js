00ae3d776fa71f7b799267c3d717f1ae88151870
Darya <darya.drazdouskaya@gmail.com>
2019-10-06 22:31:39 +0300

    feat: implement the getTimeForEducation function

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
 /**
- * @param preferences - target student focus
+ * Function that calculates how many weeks the education process would take under given conditions
+ * 
+ * @param focus - target student focus
  * @param knowsProgramming - if student can do programming and know basics
  * @param config - private student ability to perform for different focus modes
  * @returns number of weeks needed for finish education
  */
-module.exports = function getTimeForEducation(
-    focus = 'family', 
-    knowsProgramming = true,
-    config = {family: 4}
-    ) {
-      return 0;
+
+module.exports = function getTimeForEducation(focus, knowsProgramming, config) {
+    // 800 hours to master the JavaScript language, its ecosystem and related tools. 
+    const hoursProgrammingBasic = 800;
+    // 500 hours are required to learn pretty basic programming from scratch.
+    const hoursProgrammingAddition = 500;
+    const hoursProgrammingSummary = hoursProgrammingBasic + hoursProgrammingAddition;
+    const speedInHours = config[focus];
+
+    // Weeks of education process.
+    let weeks = knowsProgramming ? Math.ceil(hoursProgrammingBasic / speedInHours) : Math.ceil(hoursProgrammingSummary / speedInHours);
+
+    return weeks;
   };
-  
